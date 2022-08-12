5d39e7053601c827e9cb2b9445539a26a5f5b7c9
kumar-codes <93201503+kumar-codes@users.noreply.github.com>
2022-08-12 17:48:52 +0530

    Update RheoConnectionConstants.kt

--- products/providers/rheo-connection/src/main/kotlin/rheoconnect/entities/RheoConnectionConstants.kt
+++ products/providers/rheo-connection/src/main/kotlin/rheoconnect/entities/RheoConnectionConstants.kt
@@ -3,10 +3,10 @@ package rheoconnect.entities
 object RheoConnectionConstants {
 
   // staging
-  // const val RHEO_HOST = "https://rheo-staging-ktor.porter.in/"
+  const val RHEO_HOST = "https://rheo-staging-ktor.porter.in/"
 
   // prod
-  const val RHEO_HOST = "https://rheo-prod-ktor.internal.porter.in"
+  // const val RHEO_HOST = "https://rheo-prod-ktor.internal.porter.in"
 
   const val FEATURE_TOGGLING = "feature-toggling/get-feature-context"
 }
