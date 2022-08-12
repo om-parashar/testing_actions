cb173d079fabf5a86d6710878fcb24818714b5f3
praphul kumar <praphul.kumar@theporter.in>
2022-08-12 17:18:40 +0530

    prod oms and rheo connection

--- products/providers/oms-connection/src/main/kotlin/in.porter.calldictator.providers.omsconnect/entities/OMSConnectionConstants.kt
+++ products/providers/oms-connection/src/main/kotlin/in.porter.calldictator.providers.omsconnect/entities/OMSConnectionConstants.kt
@@ -2,7 +2,7 @@ package `in`.porter.calldictator.providers.omsconnect.entities
 
 object OMSConnectionConstants {
 
-  const val OMS_HOST_INDIA = "https://oms-staging-6.porter.in"
+  const val OMS_HOST_INDIA = "https://porter.in"
 
   const val FETCH_DRIVER_API = "/api/v1/partners/get-info"
   const val FETCH_CUSTOMER_API = "/api/v1/customers/get-info"
