532e63b3e31a88d262cc913b1ac699318cac3054
praphul kumar <praphul.kumar@theporter.in>
2022-09-08 19:43:47 +0530

    clean phone number logic before processing

--- products/product1/api/service/src/main/kotlin/in/porter/calldictator/api/service/usecases/external/CallDictationApiService.kt
+++ products/product1/api/service/src/main/kotlin/in/porter/calldictator/api/service/usecases/external/CallDictationApiService.kt
@@ -3,6 +3,7 @@ package `in`.porter.calldictator.api.service.usecases.external
 import `in`.porter.calldictator.api.models.entities.CallDictateRequest
 import `in`.porter.calldictator.api.models.entities.CallDictationResponse
 import `in`.porter.calldictator.api.service.mappers.CallDictationMapper
+import `in`.porter.calldictator.api.service.usecases.internal.ProcessRequest
 import `in`.porter.calldictator.api.service.usecases.internal.ValidateApiRequest
 import `in`.porter.calldictator.product1.domain.event.callPreProcessing.usecase.external.CallDictationService
 import javax.inject.Inject
@@ -10,6 +11,7 @@ import javax.inject.Inject
 class CallDictationApiService
 @Inject
 constructor(
+  private val processRequest: ProcessRequest,
   private val validateApiRequest: ValidateApiRequest,
   private val callDictationMapper: CallDictationMapper,
   private val callDictationService: CallDictationService
@@ -20,7 +22,7 @@ constructor(
     if (!validateApiRequest.invoke(request)) {
       return CallDictationResponse.ErrorResponse(responseCode = 4006, responseMsg = "request context not proper.")
     }
-    val callerContext = callDictationMapper.mapPreprocessingRequest(request)
+    val callerContext = processRequest.invoke(request)
     val callerResponse = callDictationService.invoke(callerContext)
     return callDictationMapper.mapPreprocessingResponse(callerResponse)
   }
