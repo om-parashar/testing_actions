2309b921b810ae554523cb86c192ab7fcbfa8aaf
om-parashar <108726108+om-parashar@users.noreply.github.com>
2022-09-14 18:59:32 +0530

    Update notify_test_coverage_failure.sh

--- scripts/notify_test_coverage_failure.sh
+++ scripts/notify_test_coverage_failure.sh
@@ -1,1 +1,2 @@
+echo "trying to notify on slack"
 curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!. Link -> https://github.com/om-parashar/testing_actions/pull/1"}' https://hooks.slack.com/services/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
