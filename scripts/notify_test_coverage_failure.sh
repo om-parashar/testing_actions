957e66bdf13b641fc1a15e1c72f26d4bda680139
Om Parashar <om.parashar@theporter.in>
2022-09-14 18:55:07 +0530

    added slack notification script

---
+++ scripts/notify_test_coverage_failure.sh
@@ -0,0 +1,1 @@
+curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!. Link -> https://github.com/om-parashar/testing_actions/pull/1"}' https://hooks.slack.com/services/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
