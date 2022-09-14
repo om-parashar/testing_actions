407fa153c57f51de532d4be0e623182524c3f815
om-parashar <108726108+om-parashar@users.noreply.github.com>
2022-09-14 23:10:49 +0530

    Update notify_test_coverage_failure.sh

--- scripts/notify_test_coverage_failure.sh
+++ scripts/notify_test_coverage_failure.sh
@@ -1,1 +1,1 @@
-curl -X POST --data-urlencode "payload={\"channel\": \"#testing-bash-script\", \"username\": \"webhookbot\", \"text\": \"This is posted to #testing-bash-script and comes from a bot named webhookbot.\", \"icon_emoji\": \":ghost:\"}" ${{secrets.SLACK_WEBHOOK_URL}}
+curl -X POST --data-urlencode "payload={\"channel\": \"#testing-bash-script\", \"username\": \"webhookbot\", \"text\": \"This is posted to #testing-bash-script and comes from a bot named webhookbot.\", \"icon_emoji\": \":ghost:\"}" $1
